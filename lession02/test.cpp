08af53189a4702e59a0d2949f7d0536327cd7e9a
yanfengneng <algsup@DESKTOP-98HDDJN.localdomain>
2022-04-30 19:38:32 +0800

    update

--- lession02/test.cpp
+++ lession02/test.cpp
@@ -1,8 +1,17 @@
 #include <iostream>
 using namespace std;
 
+int sum(int n)
+{
+    long long s = 0;
+    for (int i = 0; i < n; ++i)
+        s += i;
+    return s;
+}
+
 int main()
 {
     printf("hello world!\n");
+    cout << sum(100) << endl;
     return 0;
 }
